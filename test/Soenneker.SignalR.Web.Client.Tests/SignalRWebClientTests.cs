5b2abdffe5c822afd0891b862966b4a10e142c22
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:02:42 -0500

    TUnit conversion

--- test/Soenneker.SignalR.Web.Client.Tests/SignalRWebClientTests.cs
+++ test/Soenneker.SignalR.Web.Client.Tests/SignalRWebClientTests.cs
@@ -1,16 +1,15 @@
-using Soenneker.Tests.FixturedUnit;
-using Xunit;
+using Soenneker.Tests.HostedUnit;
 
 namespace Soenneker.SignalR.Web.Client.Tests;
 
-[Collection("Collection")]
-public class SignalRWebClientTests : FixturedUnitTest
+[ClassDataSource<Host>(Shared = SharedType.PerTestSession)]
+public class SignalRWebClientTests : HostedUnitTest
 {
-    public SignalRWebClientTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    public SignalRWebClientTests(Host host) : base(host)
     {
     }
 
-    [Fact]
+    [Test]
     public void Default()
     {
 
